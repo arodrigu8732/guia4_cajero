cf15c9cf7d9ba913111e80abdf636b9395ec9a8f
alejandro.rodriguez <alejandro.rodriguez@COArodriguez.mflatam.local>
2019-09-21 19:34:52 -0500

    #1 -> inicio del proyecto

---
+++ src/cajero2/MessageDialog.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cajero2;
+import javax.swing.JOptionPane;
+/**
+ * @author alejandro.rodriguez
+ */
+public class MessageDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Operacion Realizada Cotrrectamente");
+    }
+    
+}
